afcda98e36decf4b7435a6bcea09f9c4c65eaada
davieduardo001 <davieduardocosta01@gmail.com>
2022-02-08 18:06:05 -0300

    project createted

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace send_emails_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
