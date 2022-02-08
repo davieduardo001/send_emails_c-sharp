f77545a380a94940bfd876128acc77cb9fa678ca
davieduardo001 <davieduardocosta01@gmail.com>
2022-02-08 18:23:37 -0300

    email "body" configuration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Mail;
+using System.Text;
 
 namespace send_emails_c_
 {
@@ -6,7 +8,22 @@ namespace send_emails_c_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string email_title;
+            string email_message;
+
+            Console.WriteLine("What is the email title? ");
+            email_title = Console.ReadLine();
+
+            Console.WriteLine("What is the message? ");
+            email_message = Console.ReadLine();
+
+            MailMessage message = new MailMessage("", "");
+
+            message.Subject = "";
+            message.IsBodyHtml = true;
+            message.Body = "<p>" + email_message + "</p>";
+            message.SubjectEncoding = Encoding.GetEncoding("UTF-8");
+            message.BodyEncoding = Encoding.GetEncoding("UTF-8");
         }
     }
 }
