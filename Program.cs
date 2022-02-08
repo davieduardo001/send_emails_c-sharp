997d4eadf39a0ff484ba6b2f0d2958da3aee0602
davieduardo001 <davieduardocosta01@gmail.com>
2022-02-08 18:45:35 -0300

    finished program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 
@@ -10,6 +11,7 @@ namespace send_emails_c_
         {
             string email_title;
             string email_message;
+            string email_sender_password;
 
             Console.WriteLine("What is the email title? ");
             email_title = Console.ReadLine();
@@ -17,13 +19,38 @@ namespace send_emails_c_
             Console.WriteLine("What is the message? ");
             email_message = Console.ReadLine();
 
-            MailMessage message = new MailMessage("", "");
+            Console.WriteLine("What is the password of the sender: ");
+            email_sender_password = Console.ReadLine();
 
-            message.Subject = "";
-            message.IsBodyHtml = true;
-            message.Body = "<p>" + email_message + "</p>";
-            message.SubjectEncoding = Encoding.GetEncoding("UTF-8");
-            message.BodyEncoding = Encoding.GetEncoding("UTF-8");
+            Console.WriteLine("Press ENTER to send the email!");
+            Console.ReadLine();
+
+            try
+            { 
+                MailMessage message = new MailMessage("", ""); // sender email addressee email
+
+                message.Subject = "";
+                message.IsBodyHtml = true;
+                message.Body = "<p>" + email_message + "</p>";
+                message.SubjectEncoding = Encoding.GetEncoding("UTF-8");
+                message.BodyEncoding = Encoding.GetEncoding("UTF-8");
+
+                SmtpClient smtp_client = new SmtpClient("smtp.gmail.com", 587);
+
+                smtp_client.UseDefaultCredentials = false;
+                smtp_client.Credentials = new NetworkCredential("", ""); // Email and passoword of the sender
+
+                smtp_client.EnableSsl = true;
+
+                smtp_client.Send(message);
+
+                Console.WriteLine("The email was sent successfully! :] ");
+                Console.ReadLine();
+            }catch
+            {
+                Console.WriteLine("An error has occurred :[ ");
+                Console.ReadLine();
+            }
         }
     }
 }
